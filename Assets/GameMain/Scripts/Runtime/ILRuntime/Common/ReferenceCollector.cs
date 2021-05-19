896b77eeabcc8718a2d31565deea1fc9ede7e22d
HeNuo <1073498648@qq.com>
2021-05-19 11:09:55 +0800

    重新审视参数收集器组件

--- Assets/GameMain/Scripts/Runtime/ILRuntime/Common/ReferenceCollector.cs
+++ Assets/GameMain/Scripts/Runtime/ILRuntime/Common/ReferenceCollector.cs
@@ -12,7 +12,7 @@ namespace Sirius.Runtime
 {
     [AddComponentMenu("Game Main/Reference Collector")]   //组件列表菜单
     [DisallowMultipleComponent]
-    public sealed partial class ReferenceCollector : MonoBehaviour
+    public sealed partial class ReferenceCollector : MonoBehaviour, ISerializationCallbackReceiver
     {
         [SerializeField]
         [HideInInspector]
@@ -91,8 +91,14 @@ namespace Sirius.Runtime
             return dictGo;
         }
 
-        public void Awake()
+        public void OnBeforeSerialize()
         {
+
+        }
+
+        public void OnAfterDeserialize()
+        {
+#if UNITY_EDITOR
             m_Dict.Clear();
             for (int i = 0; i < m_ReferenceObjects.Count; i++)
             {
@@ -102,7 +108,22 @@ namespace Sirius.Runtime
                 else
                     m_Dict.Add(data.key, data.obj);
             }
+#endif
+        }
+
+        public void Awake()
+        {
 #if !UNITY_EDITOR
+            m_Dict.Clear();
+            for (int i = 0; i < m_ReferenceObjects.Count; i++)
+            {
+                ReferenceCollectorData data = m_ReferenceObjects[i];
+                if (m_Dict.ContainsKey(data.key))
+                    m_Dict[data.key] = data.obj;    //存在相同的则覆盖
+                else
+                    m_Dict.Add(data.key, data.obj);
+            }
+
                         m_ReferenceObjects.Clear();
                         m_ReferenceObjects = null;
 #endif
